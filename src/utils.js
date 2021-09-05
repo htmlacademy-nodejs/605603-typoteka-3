8bdd969239ebd2411bd0f0634484a77509cc324e
aleks.b <aleks.balev7899@gmail.com>
2021-09-05 14:15:43 +0200

    end with task 3 of module 1 typoteka

---
+++ src/utils.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports.getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (min + max + 1)) + min;
+};
+
+module.exports.shuffle = (arr) => {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const randomPosition = Math.floor(Math.random() * i);
+    [arr[i], arr[randomPosition]] = [arr[randomPosition], arr[i]];
+  }
+
+  return arr;
+};
