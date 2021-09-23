c7633076af4e2cd7aec54f1d66339f40027aabe5
aleks.b <aleks.balev7899@gmail.com>
2021-09-23 19:53:27 +0200

    module2-task2

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -8,56 +8,21 @@ const chalk = require(`chalk`);
 const DEFAULT_MOCKS_COUNT = 1;
 const MOCKS_FILE_NAME = `mocks.json`;
 const MAX_MOCKS_COUNT = 1000;
+const CATEGORIES = `categories.txt`;
+const SENTENCES = `sentences.txt`;
+const TITLES = `titles.txt`;
 
-const TITLES = [
-  `Ёлки. История деревьев`,
-  `Как перестать беспокоиться и начать жить`,
-  `Как достигнуть успеха не вставая с кресла`,
-  `Обзор новейшего смартфона`,
-  `Лучшие рок-музыканты 20-века`,
-  `Как начать программировать`,
-  `Учим HTML и CSS`,
-  `Что такое золотое сечение`,
-  `Как собрать камни бесконечности`,
-  `Рок — это протест`,
-  `Борьба с прокрастинацией`,
-  `Самый лучший музыкальный альбом этого года`,
-];
+const readFromTxt = async (fileName) => {
+  try {
+    const filePath = `./data/${fileName}`;
+    const content = await fs.readFile(filePath, `utf8`);
 
-const DESCRIPTIONS = [
-  `Ёлки — это не просто красивое дерево. Это прочная древесина.`,
-  `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
-  `Первая большая ёлка была установлена только в 1938 году.`,
-  `Вы можете достичь всего. Стоит только немного постараться и запастись книгами.`,
-  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
-  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
-  `Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
-  `Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.`,
-  `Программировать не настолько сложно, как об этом говорят.`,
-  `Простые ежедневные упражнения помогут достичь успеха.`,
-  `Это один из лучших рок-музыкантов.`,
-  `Он написал больше 30 хитов.`,
-  `Из под его пера вышло 8 платиновых альбомов.`,
-  `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.`,
-  `Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?`,
-  `Достичь успеха помогут ежедневные повторения.`,
-  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
-  `Как начать действовать? Для начала просто соберитесь.`,
-  `Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравятся только игры.`,
-  `Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`,
-];
-
-const CATEGORIES = [
-  `Программирование`,
-  `Деревья`,
-  `За жизнь`,
-  `Без рамки`,
-  `Разное`,
-  `IT`,
-  `Музыка`,
-  `Кино`,
-  `Железо`,
-];
+    return content.trim().split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
 
 const randomPublicationDate = () => {
   const randomPastThreeMonths = -getRandomInt(1, 84);
@@ -72,7 +37,7 @@ const randomPublicationDate = () => {
   });
 };
 
-const generateOffers = (count) => {
+const generateOffers = (count, sentences, categories, titles) => {
   if (count > MAX_MOCKS_COUNT) {
     console.error(chalk.red(`Не больше ${MAX_MOCKS_COUNT} публикаций`));
     process.exit(ExitCode.error);
@@ -81,20 +46,24 @@ const generateOffers = (count) => {
   return Array(count)
     .fill({})
     .map(() => ({
-      title: TITLES[getRandomInt(0, TITLES.length - 1)],
-      announce: shuffle(DESCRIPTIONS).slice(1, 5).join(` `),
-      fullText: shuffle(DESCRIPTIONS).slice(1, 5).join(` `),
+      title: titles[getRandomInt(0, titles.length - 1)],
+      announce: shuffle(sentences).slice(1, 5).join(` `),
+      fullText: shuffle(sentences).slice(1, 5).join(` `),
       createdDate: randomPublicationDate(),
-      category: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]],
+      category: [categories[getRandomInt(0, categories.length - 1)]],
     }));
 };
 
 module.exports = {
   name: `--generate`,
   async run(args) {
+    const sentences = await readFromTxt(SENTENCES);
+    const categories = await readFromTxt(CATEGORIES);
+    const titles = await readFromTxt(TITLES);
+
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_MOCKS_COUNT;
-    const content = JSON.stringify(generateOffers(countOffer));
+    const content = JSON.stringify(generateOffers(countOffer, sentences, categories, titles));
 
     try {
       await fs.writeFile(MOCKS_FILE_NAME, content);
