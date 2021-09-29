063842d08bcca4da3d99f63aab2be092ceb5b694
aleks.b <aleks.balev7899@gmail.com>
2021-09-29 21:29:03 +0200

    module3-task2

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -4,9 +4,9 @@ const {Router} = require(`express`);
 
 const articlesRouter = new Router();
 
-articlesRouter.get(`/add`, (req, res) => res.send(`/articles/add`));
-articlesRouter.get(`/category/:id`, (req, res) => res.send(`/articles/category/:id`));
-articlesRouter.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/:id`));
-articlesRouter.get(`/:id`, (req, res) => res.send(`articles/:id`));
+articlesRouter.get(`/add`, (req, res) => res.render(`articles/post`));
+articlesRouter.get(`/category/:id`, (req, res) => res.render(`articles/articles-by-category`));
+articlesRouter.get(`/edit/:id`, (req, res) => res.render(`articles/post-detail`));
+articlesRouter.get(`/:id`, (req, res) => res.render(`articles/post-detail`));
 
 module.exports = articlesRouter;
