cbac16fee256da94826aa1abee3f0d2c1b5b7e32
aleks.b <aleks.balev7899@gmail.com>
2021-09-29 20:04:10 +0200

    module3-task1

---
+++ src/express/express.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require(`express`);
+const mainRoutes = require(`./routes/main-routes`);
+const articlesRoutes = require(`./routes/articles-routes`);
+const myRoutes = require(`./routes/my-routes`);
+
+const DEFAULT_PORT = 8080;
+
+const app = express();
+
+app.use(`/`, mainRoutes);
+app.use(`/my`, myRoutes);
+app.use(`/articles`, articlesRoutes);
+
+app.listen(DEFAULT_PORT, () => {
+  console.log(`App listening at http://localhost:${DEFAULT_PORT}`);
+});
