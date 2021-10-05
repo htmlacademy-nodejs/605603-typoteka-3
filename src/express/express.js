3ae9f975e01139b42f0985929f529f40e9afe33b
aleksbalev <54625848+aleksbalev@users.noreply.github.com>
2021-10-05 20:18:55 +0200

    Личный проект: шаблонизируй это (#8)
    * module3-task1
    
    * module3-task2
    
    * change port parameters
    
    * conflict resolving
    
    * package.json/lock conflict resolve
    
    Co-authored-by: aleks.b <aleks.balev7899@gmail.com>

--- src/express/express.js
+++ src/express/express.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 const mainRoutes = require(`./routes/main-routes`);
 const articlesRoutes = require(`./routes/articles-routes`);
 const myRoutes = require(`./routes/my-routes`);
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 
 const app = express();
 
@@ -13,6 +15,16 @@ app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/articles`, articlesRoutes);
 
-app.listen(DEFAULT_PORT, () => {
-  console.log(`App listening at http://localhost:${DEFAULT_PORT}`);
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.use((req, res) => res.status(404).render(`errors/404`));
+app.use((req, res) => res.status(500).render(`errors/505`));
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
+
+const port = process.env.PORT || DEFAULT_PORT;
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
 });
